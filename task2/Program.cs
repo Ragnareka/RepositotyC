5631a1639ec43baa80a44dda0bcddd7b593e7541
Anna-Mariya Z <mantikora97@yandex.ru>
2023-03-26 08:30:52 +0400

    HomeWork 1

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.Clear();
+Console.WriteLine("вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+Console.WriteLine("Введите первое целое число.");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число.");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+    {Console.WriteLine($"max = {a}");}
+else if (b>a)
+    {Console.WriteLine($"max = {b}");}
+    else {Console.WriteLine("они равны");};
---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.WriteLine("принимает на вход три числа и выдаёт максимальное из этих чисел.");
+int [] a = new int [3];
+Console.WriteLine("Введите первое целое число.");
+ a [0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число.");
+ a [1]  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  третье целое число.");
+ a [2]  = Convert.ToInt32(Console.ReadLine());
+ int maxValue = a.Max();
+ Console.WriteLine($"Наибольшее значение {maxValue}");
+
+ 
+
+
